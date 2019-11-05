205646a70bd7824129e3bf1708e02f765d2f7edb
NtaleGrey <Shadikntale@gmail.com>
2019-11-05 22:03:04 +0300

    feature(delete order): Add functionality to delete an order
    - Add logic to delete an order
    - Write tests for this functionality
    
    [Delivers #169531044]

--- api/apps/orders/views.py
+++ api/apps/orders/views.py
@@ -1,6 +1,10 @@
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.generics import CreateAPIView, UpdateAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    UpdateAPIView,
+    DestroyAPIView,
+)
 from rest_framework.response import Response
 
 from api.apps.orders.serializers import (
@@ -61,5 +65,21 @@ class UpdateOrderStatusView(UpdateAPIView):
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
+        except Order.DoesNotExist:
+            errors.handle(errors.ORDER_2)
+
+
+class DeleteOrderView(DestroyAPIView):
+    permission_classes = (IsAuthenticated,)
+
+    def delete(self, request, *args, **kwargs):
+        order_id = kwargs.get("order_id")
+        try:
+            order = Order.objects.get(pk=order_id)
+            if order.user != request.user:
+                errors.handle(errors.ORDER_6)
+            order.delete()
+            response = {"message": "order successfully deleted!"}
+            return Response(response)
         except Order.DoesNotExist:
             errors.handle(errors.ORDER_2)
