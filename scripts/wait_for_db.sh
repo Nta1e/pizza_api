b9c187fb09bf728a823bdb08a87fee42e90e8bd9
NtaleGrey <Shadikntale@gmail.com>
2019-11-06 14:25:31 +0300

    feature(containerization): Containerize application
    - Add neccessary docker files and configs
    - Add a makefile
    
    [Delivers #169589626]

---
+++ scripts/wait_for_db.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+function wait_db() {
+python << END
+import sys
+import psycopg2
+try:
+  conn = psycopg2.connect(dbname="pizza_db", user="postgres", password="pizza-backend", host="database")
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+END
+}
+until wait_db; do
+  >&2 echo "Postgres is unavailable --> sleeping..."
+  sleep 1
+done
+>&2 echo "Postgres is up --> continuing..."
+
+./start_server.sh
